883e0ae5754e3a471f5818a8290cf8d7f5a2b25f
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-08-23 00:10:55 +0100

    Add files via upload

---
+++ custom_documents/models/folder.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+class DocumentFolder(models.Model):
+    _description = 'Document folder'
+    _inherit = 'documents.folder' 
+    admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
+    active=fields.Boolean('Active', default=True)
+    def custom_groups(self):
+        folders=self.env['documents.folder'].search([])
+        for folder in folders:
+            if folder.parent_folder_id:
+                parent_folder_id=folder.parent_folder_id.id
+                #raise UserError(_(folder.group_ids))
+                parent_folder=self.env['documents.folder'].search([('id','=',parent_folder_id)])
+                if folder.read_group_ids:
+                    for id in [group.id for group in  folder.read_group_ids ]:
+                        parent_folder.write({'read_group_ids':[(4,id)]})
+                if folder.group_ids:
+                    for id in [group.id for group in  folder.group_ids ]:
+                        parent_folder.write({'read_group_ids':[(4,id)]})
+        return()
+"""class SignSendRequest(models.Model):
+    _description = 'Sign Send Request'
+    _inherit = 'sign.send.request' """
+
