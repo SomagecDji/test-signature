5ecb46dfe6f30036ce69cc0d17729644a100cea6
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-09-04 00:51:44 +0100

    Update inherit_project_workspace.py

--- custom_documents/wizards/inherit_project_workspace.py
+++ custom_documents/wizards/inherit_project_workspace.py
@@ -12,7 +12,7 @@ class FolderInheritProjectWorkspace(models.TransientModel):
           document_folder=self.env['documents.folder']
           document_parent_0=documet_folder.create({'name':self.project_name})
           all_subfolders0=self.env['documents.folder'].search([('parent_folder_id','=','main_workspace_id')])
-          if len(all_subfolders0)!=0
+          if len(all_subfolders0)!=0:
               for i in all_subfolders0:
                 document_folder=self.env['documents.folder']
                 document_parent_1=documet_folder.create({'name':i.name,'parent_folder_id':document_parent_0.id})
