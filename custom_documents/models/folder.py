7db8fcc9f541cf4d65fabd9c061ad191c706538c
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-09-05 21:34:08 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -34,17 +34,17 @@ class DocumentFolder(models.Model):
     def inherit_workspace(self):
         main_workspace_id=self.env['documents.folder'].search([('id','=',3)]).id
         document_folder=self.env['documents.folder']
-        document_parent_0=documet_folder.create({'name':self.project_name})
+        document_parent_0=document_folder.create({'name':self.project_name})
         all_subfolders0=self.env['documents.folder'].search([('parent_folder_id','=','main_workspace_id')])
         if len(all_subfolders0)!=0:
             for i in all_subfolders0:
                 document_folder=self.env['documents.folder']
-                document_parent_1=documet_folder.create({'name':i.name,'parent_folder_id':document_parent_0.id})
+                document_parent_1=document_folder.create({'name':i.name,'parent_folder_id':document_parent_0.id})
                 all_subfolders1=self.env['documents.folder'].search([('parent_folder_id','=',i.id)])
                 if len(all_subfolders1)!=0:
                     for j in all_subfolders1:
                         document_folder=self.env['documents.folder']
-                        document_parent_2=documet_folder.create({'name':j.name,'parent_folder_id':document_parent_0.id})
+                        document_parent_2=document_folder.create({'name':j.name,'parent_folder_id':document_parent_0.id})
                         all_subfolders2=self.env['documents.folder'].search([('id','=',j.id)])
         return {'type': 'ir.actions.act_window_close'}     
 """class SignSendRequest(models.Model):
