a1d58b1a53136cc6269e73760f3ac2ec845f2638
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-09-05 19:56:17 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -22,7 +22,7 @@ class DocumentFolder(models.Model):
                         parent_folder.write({'read_group_ids':[(4,id)]})
         return()
     def view_inherit_workspace(self):
-        form_view = self.env.ref('akwa_purchase.current_rejection_remark_bc_form_view')
+        form_view = self.env.ref('documents_folder.inherit_workspace_form_view')
         self.ensure_one()
         return {'name': _('Merci de saisir le nom du projet:'),
                 'type': 'ir.actions.act_window',
@@ -31,6 +31,22 @@ class DocumentFolder(models.Model):
                 'view_id': form_view.id,
                 'res_id': self.id,
                 'target': 'new'}
+    def inherit_workspace(self):
+        main_workspace_id=self.env['documents.folder'].search([('id','=',3)]).id
+        document_folder=self.env['documents.folder']
+        document_parent_0=documet_folder.create({'name':self.project_name})
+        all_subfolders0=self.env['documents.folder'].search([('parent_folder_id','=','main_workspace_id')])
+        if len(all_subfolders0)!=0:
+            for i in all_subfolders0:
+            document_folder=self.env['documents.folder']
+            document_parent_1=documet_folder.create({'name':i.name,'parent_folder_id':document_parent_0.id})
+            all_subfolders1=self.env['documents.folder'].search([('parent_folder_id','=',i.id)])
+            if len(all_subfolders1)!=0:
+                for j in all_subfolders1:
+                document_folder=self.env['documents.folder']
+                document_parent_2=documet_folder.create({'name':j.name,'parent_folder_id':document_parent_0.id})
+                all_subfolders2=self.env['documents.folder'].search([('id','=',j.id)])
+        return {'type': 'ir.actions.act_window_close'}     
 """class SignSendRequest(models.Model):
     _description = 'Sign Send Request'
     _inherit = 'sign.send.request' """
